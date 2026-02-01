32f7afdb1d7768db1db5257fc50143da8fcc52b3
Pierce Broudin <piercebroudin.pb@gmail.com>
2026-02-01 17:53:32 +0100

    chore: [US13] - configure Testcontainers

--- backend/src/test/java/com/c15tour/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/c15tour/backend/BackendApplicationTests.java
@@ -2,12 +2,21 @@ package com.c15tour.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
+@Testcontainers
 class BackendApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
 	}
-
+	
 }
